f9148342d545e7a62d07cf5f4ff34ee52e75fed8
Ale-Venegas-L <paul.venegas@duocuc.cl>
2025-09-05 10:28:48 -0400

    avances en login (html, css y js)

---
+++ assets/js/login.js
@@ -0,0 +1,14 @@
+//valores a validar: run, nombre, apellido, correo, contraseña, telefono, seleccion de region y comuna
+const run = document.getElementById('run').value().trim()
+const nombre = document.getElementById('name').value().trim()
+const apellido = document.getElementById('surname').value().trim()
+const correo = document.getElementById('email').value().trim()
+const contraseña = document.getElementById('psswd').value().trim()
+const contraseñaV = document.getElementById('psswd2').value().trim()
+const telefono = document.getElementById(cel).value().trim()
+const correoDir = document.getElementById('email-dist')
+const region = document.getElementById('region')
+const comuna = document.getElementById('comuna')
+
+
+        
